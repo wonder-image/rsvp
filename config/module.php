1dfd7b3b2fc6a51924b94f59400dcd0b56b88e02
Andrea Marinoni <marinoni@wonderimage.it>
2026-06-23 15:48:48 +0200

    feat(rsvp): register rsvp component namespace on module boot

--- config/module.php
+++ config/module.php
@@ -4,10 +4,12 @@ use Wonder\Consent\LegalDocumentTypeContext;
 use Wonder\Localization\LanguageContext;
 use Wonder\Plugin\Rsvp\Rsvp;
 use Wonder\Plugin\Rsvp\Services\LegalDocumentSeeder;
+use Wonder\View\ComponentNamespaceRegistry;
 
 LegalDocumentTypeContext::addType('image_release', 'Liberatoria immagini');
 LanguageContext::addUrlsPath(Rsvp::langPath());
 LegalDocumentSeeder::ensureImageRelease();
+ComponentNamespaceRegistry::register('rsvp', Rsvp::root().'/view/components');
 
 return [
     'legal_documents' => [
